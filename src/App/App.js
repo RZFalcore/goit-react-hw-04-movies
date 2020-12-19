489d2cb98b8ac609c4dbe8067c269c6a8aa27971
Eugene Roy <box.rz.box@gmail.com>
2020-12-19 19:32:18 +0200

    api

--- src/App/App.js
+++ src/App/App.js
@@ -4,6 +4,7 @@ import "./App.module.css";
 export default class App extends Component {
   state = {};
   render() {
+    console.log(process.env.REACT_APP_API_KEY);
     return <h1>App</h1>;
   }
 }
